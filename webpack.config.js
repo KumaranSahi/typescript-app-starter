dbd5a87b160683ba410d9e4f5891cfdcbcba0733
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-05-24 20:38:35 +0530

    Able to build and server HTML and js files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+const source = path.resolve(__dirname, "src","index.js");
+const build = path.resolve(__dirname, "build");
+
+const webpackConfig = {
+  entry: source,
+  output: {
+    filename: "[name].bundle.js",
+    path: build,
+    clean:true
+  },
+  mode: "development",
+  plugins:[
+      new htmlWebpackPlugin({
+          template: path.resolve(__dirname,"src","index.html")
+      })
+  ]
+};
+
+module.exports = webpackConfig;
